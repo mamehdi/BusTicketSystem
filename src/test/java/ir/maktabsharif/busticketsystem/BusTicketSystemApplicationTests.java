dceff2dab823ac42327ec255c784d47226d69c7a
mamehdi <mamehdiegold@yahoo.com>
2025-12-01 23:33:21 +0330

    Initial commit

---
+++ src/test/java/ir/maktabsharif/busticketsystem/BusTicketSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.maktabsharif.busticketsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusTicketSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
