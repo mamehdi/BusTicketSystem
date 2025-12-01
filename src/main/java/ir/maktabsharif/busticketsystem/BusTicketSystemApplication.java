3324f34cd708ed74264b90a2770170b30da4d7d1
mamehdi <mamehdiegold@yahoo.com>
2025-12-02 00:24:07 +0330

    review for warning

--- src/main/java/ir/maktabsharif/busticketsystem/BusTicketSystemApplication.java
+++ src/main/java/ir/maktabsharif/busticketsystem/BusTicketSystemApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BusTicketSystemApplication {
 
-    public static void main(String[] args) {
+    static void main(String[] args) {
         SpringApplication.run(BusTicketSystemApplication.class, args);
     }
 
--- src/main/java/ir/maktabsharif/busticketsystem/enums/Gender.java
+++ src/main/java/ir/maktabsharif/busticketsystem/enums/Gender.java
@@ -1,5 +1,5 @@
 package ir.maktabsharif.busticketsystem.enums;
 
 public enum Gender {
-    MALE,FEMALE,OTHER;
+    MALE,FEMALE,OTHER
 }
